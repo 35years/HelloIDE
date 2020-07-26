5720db0d2bb645a2c9ba3514f30df10c229cbfa3
qk <2500813866@qq.com>
2020-07-26 13:50:39 +0800

    I22222 commit

---
+++ Hello Moudle/src/com/qk/java/Hello.java
@@ -0,0 +1,8 @@
+package com.qk.java;
+
+public class Hello {
+//    单行注释
+    public static void main(String[] args) {
+        System.out.println("hello moudle");
+    }
+}
---
+++ src/com/qk/java/Hello.java
@@ -0,0 +1,16 @@
+package com.qk.java;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello idea");
+        System.out.println("hello idea");
+
+
+
+
+
+
+
+
+    }
+}
